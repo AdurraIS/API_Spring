1241276ebb4bc022bd672849a6ab0428db1c6eba
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-11 19:16:54 -0300

    Init Project

---
+++ src/main/java/com/giovanni/apisgp/ApisgpApplication.java
@@ -0,0 +1,13 @@
+package com.giovanni.apisgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisgpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giovanni/apisgp/ApisgpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanni.apisgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
